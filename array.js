6e1d5ec24826ee18479eb6457cb130f4b010ffc0
Imanajeeb11 <imanajeeb11@gmail.com>
2025-01-30 08:05:20 +0530

    Array.js File created and learnt arr length

---
+++ array.js
@@ -0,0 +1,18 @@
+const students=["Aditi", "Aakash", "Amit",{},[],""];
+console.log(students);
+console.log(students[1]);
+const length_of_students_array=students.length;
+console.log("Length of array-->", length_of_students_array);
+console.log(students[length_of_students_array]); //undefined
+console.log("Last element of array-->",students[length_of_students_array-1]); //last element is an empty string
+
+
+//const students1:string=["Aditi", "Aakash", "Amit"]; //typescript added: it can only have elements with type String
+
+vehicle = { type: "Car", model: "Toyota", year: 2020 };
+const values=(Object.values(vehicle));
+const keys=(Object.keys(vehicle));
+console.log(typeof values); //object array
+const length=values.length;
+console.log("No. of elements in object-->",length);
+
