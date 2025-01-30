47a2dd45e27da2a382599adbbeb184c949535128
Imanajeeb11 <imanajeeb11@gmail.com>
2025-01-30 08:16:35 +0530

    Array.js File updated

--- array.js
+++ array.js
@@ -1,3 +1,9 @@
+/*4 steps: 
+1. Declaration
+2. Initialization
+3. Access/Read
+4. Update/Mutate/Assign/Change */
+
 const students=["Aditi", "Aakash", "Amit",{},[],""];
 console.log(students);
 console.log(students[1]);
@@ -16,3 +22,14 @@ console.log(typeof values); //object array
 const length=values.length;
 console.log("No. of elements in object-->",length);
 
+//Playing with an empty array
+const arr=[];
+console.log(arr.length); //zero
+arr[2]="Ima";
+console.log(arr); //[ <2 empty items>, 'Ima' ]
+console.log(arr.length);
+console.log("TypeOf Array-->",typeof(arr));
+console.log(arr[0]+arr[1]+arr[2]); //NaNIma
+arr[0]=2+1;
+arr[1]=function total(){console.log(arr.length);};
+console.log(arr[0]+arr[1]+arr[2]); //3function total(){console.log(arr.length);}Ima
