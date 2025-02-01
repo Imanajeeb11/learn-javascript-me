384919f3c30852bb23453648b8fdb5be12b52035
Imanajeeb11 <imanajeeb11@gmail.com>
2025-02-01 09:10:51 +0530

    added function.js and .DS_Store files

--- function.js
+++ function.js
@@ -8,6 +8,10 @@ function <name> (arg1, arg2){
 <return/no return>
 }
 */
+//const h = getName1({name:"Hello2"}); // will give an error - as we can't invoke a function-expression/arrow function before function declaration
+const newName1 = getName2;
+console.log("getName2-->",newName1);
+
 function myfunc(){
 console.log("This is my statement inside a function");
 return 0;
@@ -94,3 +98,34 @@ const val211 = (function stop11(){
 }  )();
 
 console.log(val211);
+
+const person ={name:"Ima", age:25};
+function getName(o){
+  return o.name; //shows this is an object
+}
+
+const n = getName(person);
+const f = getName({name:"Hello", age:25});
+console.log(n);
+console.log(f);
+
+//Arrow function
+
+//1.normal way
+
+function getName2(o){
+  return o.name; 
+}
+
+ //2. function expression way
+/*
+const getName = function(o){
+  return o.name;
+}*/
+
+//3. arrow function way
+const getName1 = (o) => { //this is an arrow function
+  return o.name;
+}
+//const g = getName1({name:"Hello2"});
+console.log(getName1);
