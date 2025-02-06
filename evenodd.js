375492658b587ba577b30df068c55a84ff30e88f
Imanajeeb11 <imanajeeb11@gmail.com>
2025-02-06 07:37:23 +0530

    evenodd.js

---
+++ evenodd.js
@@ -0,0 +1,19 @@
+const evenOrOdd = (num) => {
+if(num%2==0){
+console.log(`${num} is an even number`);
+}
+else{
+    console.log(`${num} is an odd number`);
+}
+};
+evenOrOdd(4);
+
+const isDivisiblebyThree = (num) => {
+    if(num%3==0){
+        console.log(`${num} is divisible by 3`);
+        }
+        else{
+            console.log(`${num} is not divisible by 3`);
+        }  
+};
+isDivisiblebyThree(6);
